30c389bd22b43efd9597a17df5d89e62791565d8
root <abejarano@pinttosoft.com>
2016-03-27 11:19:35 -0430

    manda a eliminar paginas

--- Applications/View/paginas/vista-paginas.php
+++ Applications/View/paginas/vista-paginas.php
@@ -10,6 +10,10 @@
 <div id="right">
     <?php
         if($Spry->getParameterUrl(2) == 'listado'){
+            if($Spry->getParameterUrl(3) == "eliminar" && is_numeric($Spry->getParameterUrl(4))){
+                $Spry->Services()->Controller->setDelete($Spry->getParameterUrl(4));
+            }
+
             include 'listado.php';
         }else{
             if($Spry->getParameterUrl(2) =='buscar' && is_numeric($Spry->getParameterUrl(3))){
