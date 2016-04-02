03cb9086dea8bdf935ff3f27cd7d9c3a743ae9f2
root <abejarano@pinttosoft.com>
2016-04-02 13:46:25 -0430

    para subir imagenes

---
+++ Public/js/publicador.js
@@ -0,0 +1,81 @@
+var IMG_EDITOR = 0;
+tinymce.init({
+    selector: "textarea#textarea-descripcion",
+    language : "es",
+    plugins: ["image"],
+    file_browser_callback: function(field_name, url, type, win) {
+        if(type=='image'){
+            $('#my_form input').click();
+        }
+    },
+    fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
+    relative_urls: true,
+    toolbar_items_size: "medium",
+    schema: "html5",
+    menubar: 'insert view format',
+    fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+    // Plugins
+    plugins : "table link charmap autolink lists preview media textcolor image",
+    // Toolbar
+    toolbar1: "bold italic underline strikethrough | forecolor | backcolor | fontsizeselect | aligncenter alignleft alignjustify | formatselect | bullist numlist",
+    toolbar2: "outdent indent | undo redo | link unlink anchor video_template_callback charmap | preview",
+    height: "400",
+    inline: false
+});
+$(function(){
+    $("#imagen_upload2").on("change", function(e){
+        if(IMG_EDITOR > 6){
+            mostrarMensajes("Ha superado la cantidad de imagenes permitidas");
+            return false;
+        }
+        //$('#progress').show();
+        e.preventDefault();
+        //$('#img-ajax').show();
+        var formData = new FormData($("#my_form")[0]);
+        var ruta = "/Applications/Ajax/SubirImagen.php";
+
+        $.ajax({
+            url: ruta,
+            type: "POST",
+            dataType: "json",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+
+            xhr: function() {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    myXhr.upload.addEventListener('progress',function(ev) {
+                        if (ev.lengthComputable) {
+                            var percentUploaded = Math.floor(ev.loaded * 100 / ev.total);
+                            //console.log('Cargando archivo '+percentUploaded+'%');
+                            /*$('#progress').val(percentUploaded);
+                             $('#porcentaje').text(percentUploaded+"%");*/
+                            // update UI to reflect percentUploaded
+                        } else {
+                            console.info('Uploaded '+ev.loaded+' bytes');
+                            // update UI to reflect bytes uploaded
+                        }
+                    }, false);
+                }
+                return myXhr;
+            },
+            success: function(datos){
+                //$('#progress').hide();
+                if(datos.message != "bien"){
+                    alert(datos.message);
+                }else{
+                    tinyMCE.execCommand('mceInsertContent',false,'<br>'+datos.archivo)
+                    IMG_EDITOR = IMG_EDITOR  + 1;
+                }
+            },
+            error: function (err) {
+                //$('#barra_progreso').hide();
+                alert(err.responseText);
+                //$('#img-ajax').hide();
+            }
+
+        })
+    });
+})
---
+++ Public/js/upload.js
@@ -0,0 +1,112 @@
+var input=document.createElement('input');
+input.type="file";
+
+var objHtml;
+//$(input).click();
+
+$(function(){
+    $(input).change(function(e){
+        handleFileSelect(e);
+    });
+});
+function uploadImage(obj){
+    objHtml = obj;
+    $(input).click();
+}
+function handleFileSelect(evt) {
+
+    var files = evt.target.files[0]; // FileList object
+    if(files.type != 'image/jpg' || files.type != 'image/jpeg' || files.type != 'image/png' || files.type != 'image/gif'){
+        alert("Formato de archivo no permitido");
+        return false;
+    }
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+        return function(e) {
+
+            var image = new Image();
+            image.src = e.target.result;
+            image.onload = function() {
+                if(image.width < 400){
+                    alert("Por favor ingrese una imagen mas grande");
+                    return false;
+                }else if(image.height < 300){
+                    alert("Por favor ingrese una imagen mas grande");
+                    return false;
+                }
+                //console.log(image.width);
+                var canvas = document.createElement('canvas');
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(image, 0, 0);
+                var MAX_WIDTH = 800;
+                var MAX_HEIGHT = 800;
+                var width = image.width;
+                var height = image.height;
+
+                if (width > height) {
+                    if (width > MAX_WIDTH) {
+                        height *= MAX_WIDTH / width;
+                        width = MAX_WIDTH;
+                    }
+                } else {
+                    if (height > MAX_HEIGHT) {
+                        width *= MAX_HEIGHT / height;
+                        height = MAX_HEIGHT;
+                    }
+                }
+                canvas.width = width;
+                canvas.height = height;
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(this, 0, 0, width, height);
+
+                var dataurl = canvas.toDataURL("image/jpg");
+                /*es la imagen que se muestra en el primer momento que cargan el formulario de subir imagenes*/
+                var imgReferencia = $('#'+objHtml+" img").attr('src');
+                $('#'+objHtml+" img").attr('src',dataurl);
+
+                document.getElementById('hidden_data').value = dataurl;
+                var fd = new FormData(document.forms["form1"]);
+
+                //var formElement = document.querySelector("form");
+                $('#barra-progreso').show();
+                if (window.XMLHttpRequest){
+                    var xmlhttp = new XMLHttpRequest();
+                }else{
+                    var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+
+                xmlhttp.open("POST", "/Applications/Ajax/SubirImagen.php");
+                xmlhttp.upload.onprogress = function(e) {
+                    if (e.lengthComputable) {
+                        var percentUploaded = Math.floor(e.loaded * 100 / e.total);
+                        //console.log('Cargando archivo '+percentUploaded+'%');
+                        $('#barra-progreso progress').val(percentUploaded);
+                        $('#barra-progreso p').text(percentUploaded+"%");
+
+                    }
+                };
+                xmlhttp.send(fd);
+                xmlhttp.onreadystatechange = function() {
+                    $('#barra-progreso').hide();
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        $('#barra-progreso').hide();
+                        alert(xmlhttp.responseText);
+                        element = '<input type = "hidden" name = "galeria_image[]" values = "'+xmlhttp.responseText+'">  ';
+                        $('#'+objHtml).append(element);
+                    }else if(xmlhttp.status == 413){
+                        $('#'+objHtml+" img").attr('src',imgReferencia);
+                        alert('Imagen no soportada, es muy grande');
+
+                    }
+                };
+
+
+            };
+        };
+    })(files);
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(files);
+
+    //}
+}
