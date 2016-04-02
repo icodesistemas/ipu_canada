ca80ed7a981437f826401029f2d43f30eebe80fc
root <abejarano@pinttosoft.com>
2016-04-02 07:26:18 -0430

    se coloco el campo de pk pagina padre para poder tener menus jerarquicos

--- Applications/View/paginas/agregar-actualizar.php
+++ Applications/View/paginas/agregar-actualizar.php
@@ -14,8 +14,8 @@
     <input type="hidden" name="pk" value="<?= (isset($rs["pk_pagina"]) ? $rs["pk_pagina"] : '') ?>">
 
 
-    <section style="width:70%; float:left;">
-        <div class="input-control text" style="width: 91%">
+    <section style="width:80%; float:left;">
+        <div class="input-control text" style="width: 100%">
             <label>Sección</label>
             <input type="text" autofocus="on" placeholder="Nombre o Título de la Sección" value = "<?= (isset($rs["pagina"]) ? $rs["pagina"] : '') ?>" name="seccion" data-validate-func="required"
                    data-validate-hine="This field can not be empty!">
@@ -26,19 +26,14 @@
         <br><br>
         <div class="input-control text" style="width: 44%">
             <label>Título</label>
-            <input type="text" placeholder="Título de la sección" value="<?= (isset($rs["titulo"]) ? $rs["titulo"] : '') ?>" name="titulo" required="required">
+            <input type="text" placeholder="Título de la sección" value="<?= (isset($rs["titulo"]) ? $rs["titulo"] : '') ?>" name="titulo" required="required" data-validate-func="required"
+                   data-validate-hine="This field can not be empty!">
         </div>
-        <div class="input-control text" style="width: 44%">
-            <label>Descripción</label>
-            <input type="text" placeholder="Descripcion de la razon de la sección" value="<?=(isset($rs["descripcion"]) ? $rs["descripcion"] : '') ?>" maxlength="200" name="descrip" required="required">
-        </div>
-
-        <div class="clear"></div>
-        <br><br>
         <div class="input-control text">
             <label>URL</label>
             <input type="text" value="<?=(isset($rs["url"]) ? $rs["url"] : '') ?>" name="URL">
         </div>
+
         <div class="input-control text">
             <label>Abrir URL en:</label>
             <select name="open-URL" id = "open-URL">
@@ -46,11 +41,31 @@
                 <option value="_blank">Otra Venta</option>
             </select>
         </div>
+        <div class="clear"></div>
+        <br><br>
+        <div class="input-control text" style="width: 100%">
+            <label>Descripción</label>
+            <input type="text" placeholder="Descripcion de la razon de la sección" value="<?=(isset($rs["descripcion"]) ? $rs["descripcion"] : '') ?>" maxlength="200" name="descrip" required="required" data-validate-func="required"
+                   data-validate-hine="This field can not be empty!">
+        </div>
+        <div class="clear"></div>
+        <br>
         <label class="switch">Sección Activa?</label>
         <label class="switch">
             <input type="checkbox" name="pagina-activa" <?=(isset($rs["status"]) ? 'checked = "checked"' : '') ?>>
             <span class="check"></span>
         </label>
+        <div class="input-control text">
+            <label>Página padre</label>
+            <select name="cb-padre" id="cb-padre">
+                <option value="/">Raiz</option>
+                <?php
+                    $where = ' status = "A" ';
+                    echo $Spry->Services()->Functions->getLoadCombo('tb_paginas', 'pk_pagina, pagina',$where);
+                ?>
+            </select>
+        </div>
+
         <div class="clear"></div>
     </section>
 </form>
@@ -62,6 +77,9 @@
         <?php
             if(isset($rs["abrir"]) && !empty($rs["abrir"])){
                 echo '$("#open-URL option[value='.$rs["abrir"].']").attr("selected",true);';
+            }
+            if(isset($rs["pk_pagina_padre"])){
+                echo '$("#cb-padre option[value='.$rs["pk_pagina_padre"].']").attr("selected",true);';
             }
         ?>
     })
